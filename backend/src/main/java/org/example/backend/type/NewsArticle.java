7d58d238fd6695d9ec096416fddd1c313e7da16f
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-28 16:43:41 +0200

    formatted the published date of the articles to a more readable format and displayed it on the article card

--- backend/src/main/java/org/example/backend/type/NewsArticle.java
+++ backend/src/main/java/org/example/backend/type/NewsArticle.java
@@ -1,12 +1,12 @@
 package org.example.backend.type;
 
-import java.util.UUID;
-
 public record NewsArticle(
         Source source,
         String author,
         String title,
         String description,
-        String url
+        String url,
+        String urlToImage,
+        String publishedAt
 ) {
 }
--- frontend/src/assets/components/ArticleCard.tsx
+++ frontend/src/assets/components/ArticleCard.tsx
@@ -4,6 +4,18 @@ type ArticleCardProps = {
     article:article
 }
 export default function ArticleCard({article}:ArticleCardProps) {
+    const published_date = new Date(article.publishedAt)
+    const published_date_milliseconds = new Date(article.publishedAt).getTime();
+    const today = Date.now();
+    const difference = Math.round((((today - published_date_milliseconds)/60000)/60)/24)
+
+
+    const germanTimeString = published_date.toLocaleString("de-DE", {
+        timeZone: "Europe/Berlin",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 
     return (
         <div className="article-card">
@@ -12,7 +24,15 @@ export default function ArticleCard({article}:ArticleCardProps) {
             </div>
             <div className="article-topic">{article.description}</div>
             <div className="article-date">
-                <span>Gestern, 20:00</span>
+                {
+                    difference === 1 ?
+                    <span>Gestern, {published_date.getHours().toString().padStart(2, '0')}:{published_date.getSeconds().toString().padStart(2, '0')}</span>
+                : (difference === 0) ?
+                    <span>Heute, {published_date.getHours().toString().padStart(2, '0')}:{published_date.getSeconds().toString().padStart(2, '0')}</span>
+               :
+                    <span>{germanTimeString}</span>
+                }
+
             </div>
         </div>
     )
