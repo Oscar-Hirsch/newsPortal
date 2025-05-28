2828a4f66b39a2ee5a8487150cbd5f8660d0053e
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-28 10:16:13 +0200

    change css

--- frontend/src/assets/pages/ArticlePage.tsx
+++ frontend/src/assets/pages/ArticlePage.tsx
@@ -15,7 +15,6 @@ export default function ArticlePage({allArticles}:ArticlePageProps) {
         <>
             <Header></Header>
             <MainArticle articles={filteredArticles}></MainArticle>
-
         </>
     )
 }
