0f97266efce5bdaa228934019ea92b896a03a403
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-28 13:44:39 +0200

    add searchbar to header conditionally when the current page is not the homepage

--- frontend/src/assets/components/Header.tsx
+++ frontend/src/assets/components/Header.tsx
@@ -1,5 +1,19 @@
-export default function Header() {
+import SearchBar from "./SearchBar.tsx";
+
+type HeaderProps = {
+    update:()=>void
+}
+export default function Header({update}:HeaderProps) {
     return (
-        <div className={"header"}></div>
+        <div className={"header-wrapper"}>
+            <div className={"header"}>
+                <div className="logo">MediaSource</div>
+                { window.location.pathname !== "/home"
+                    ? <SearchBar updateCallBack={update}/>
+                    : null
+
+                }
+            </div>
+        </div>
     )
 }
