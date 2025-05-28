23160eafe55bdf3540ebd83297c19b35b34ed615
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-28 09:29:39 +0200

    deleted unused contexts

--- frontend/src/assets/contexts/contexts.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const SearchContext = createContext<{
-    searchString: string;
-    setSearchString: (value: string) => void;
-}>({
-    searchString: "",
-    setSearchString: () => {},
-});
