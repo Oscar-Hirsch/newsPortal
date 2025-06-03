49ba5cb72662aa7df060e22eeb5a5d8e34c6c570
Oscar-Hirsch <oscar@completeaudio.com>
2025-06-03 14:41:26 +0200

    create detail page without css

--- backend/src/main/java/org/example/backend/type/NewsArticle.java
+++ backend/src/main/java/org/example/backend/type/NewsArticle.java
@@ -1,12 +1,13 @@
 package org.example.backend.type;
 
-import java.util.UUID;
-
 public record NewsArticle(
+        String id,
         Source source,
         String author,
         String title,
         String description,
-        String url
+        String publishedAt,
+        String url,
+        String urlToImage
 ) {
 }
