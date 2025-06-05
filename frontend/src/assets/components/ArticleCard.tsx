56436b5bfc591de4b78c06726cde9d82224060d0
Oscar-Hirsch <oscar@completeaudio.com>
2025-06-05 15:23:31 +0200

    add picture to article card

--- frontend/src/assets/components/ArticleCard.tsx
+++ frontend/src/assets/components/ArticleCard.tsx
@@ -8,12 +8,19 @@ export default function ArticleCard({article}:ArticleCardProps) {
     const navigate = useNavigate()
 
     return (
-        <div className="article-card" onClick={()=>navigate(`/${article.id}`)}>
-            <div className="source-header">
-                <div className="source-name">{article.source.name}</div>
+        <div className="bg-white flex flex-col rounded-xl p-6 shadow transition-shadow duration-200 ease-in-out hover:shadow-lg hover:-translate-y-1" onClick={()=>navigate(`/${article.id}`)}>
+            <div className="flex justify-between items-center mb-4">
+                <div className="font-semibold text-[1.1rem]">{article.source.name}</div>
             </div>
-            <div className="article-topic">{article.description}</div>
-            <div className="article-date">
+            <div className="line-clamp-2 text-[1rem] font-semibold mb-4 text-slate-800">{article.title}</div>
+            <div className="w-full h-48 overflow-hidden rounded-t-lg">
+                <img
+                    src={article.urlToImage}
+                    alt={article.title}
+                    className="w-full h-full object-cover"
+                />
+            </div>
+            <div className="flex justify-between items-center text-sm text-gray-500 mt-auto">
                 <span>{article.publishedAt}</span>
             </div>
         </div>
